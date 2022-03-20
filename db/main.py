093dc9323bacfcbff454f5aabe0ebc70aada5d89
PigeonsHouse <bullbull1230@gmail.com>
2022-03-20 17:18:01 +0900

    [update] herokuのDBのURL対応

--- db/main.py
+++ db/main.py
@@ -5,14 +5,14 @@ from sqlalchemy.orm import sessionmaker, Session
 
 load_dotenv()
 
-DATABASE = 'postgresql'
-USER = os.environ.get('POSTGRES_USER')
-PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-HOST = os.environ.get('POSTGRES_HOST')
-PORT = os.environ.get('POSTGRES_PORT', 5432)
-DB_NAME = os.environ.get('POSTGRES_DB')
+# DATABASE = 'postgresql'
+# USER = os.environ.get('POSTGRES_USER')
+# PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+# HOST = os.environ.get('POSTGRES_HOST')
+# PORT = os.environ.get('POSTGRES_PORT', 5432)
+# DB_NAME = os.environ.get('POSTGRES_DB')
 
-DATABASE_URL = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME)
+DATABASE_URL = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 
 engine = create_engine(DATABASE_URL)
 
